e9175783cc8ec8d4d229314ae30883dd3d5846f8
Burenkov Oleg <duncanscoffee@gmail.com>
2024-02-05 12:35:50 +0200

    Created tests for JUnit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Test;
+
 public class CashbackHackServiceTest {
     @org.junit.Test
         public void testRemain() {
@@ -36,5 +38,41 @@ public class CashbackHackServiceTest {
 
             org.junit.Assert.assertEquals(expected, actual);
         }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainBoundaryEqualAmount2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainAmountWithMinus2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = -500;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        org.junit.Assert.assertEquals(expected, actual);
+    }
 
 }
